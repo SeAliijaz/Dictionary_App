d1c167b618ca5717e239a6f10ce5515a15f1b820
Ali ijaz <Se.Aliijaz@gmail.com>
2022-05-25 23:25:32 +0500

    Final setup

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+    final Size s = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dictionary App'),
+      ),
+      body: Container(
+        height: s.height,
+        width: s.width,
+        child: Column(
+          children: [
+            Text('Dictionary App'),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Dictionary App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
       home: HomeScreen(),
     );
   }
